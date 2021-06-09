615969d00e32751a66ef8803ffd95eaa48ad48d2
xcmyz <xcmyz@outlook.com>
2021-06-10 00:35:38 +0800

    Fix bug

--- utils.py
+++ utils.py
@@ -170,24 +170,23 @@ def pad(input_ele, mel_max_length=None):
 
 
 def overlap_and_add(signal, frame_step):
-    """Reconstructs a signal from a framed representation.
+    """
+    Reconstructs a signal from a framed representation.
 
     Adds potentially overlapping frames of a signal with shape
     `[..., frames, frame_length]`, offsetting subsequent frames by `frame_step`.
     The resulting tensor has shape `[..., output_size]` where
-
         output_size = (frames - 1) * frame_step + frame_length
 
     Args:
         signal: A [..., frames, frame_length] Tensor. All dimensions may be unknown, and rank must be at least 2.
         frame_step: An integer denoting overlap offsets. Must be less than or equal to frame_length.
-
     Returns:
         A Tensor with shape [..., output_size] containing the overlap-added frames of signal's inner-most two dimensions.
         output_size = (frames - 1) * frame_step + frame_length
-
     Based on https://github.com/tensorflow/tensorflow/blob/r1.12/tensorflow/contrib/signal/python/ops/reconstruction_ops.py
     """
+
     outer_dimensions = signal.size()[:-2]
     frames, frame_length = signal.size()[-2:]
 
@@ -200,13 +199,13 @@ def overlap_and_add(signal, frame_step):
 
     subframe_signal = signal.view(*outer_dimensions, -1, subframe_length)
 
-    frame = torch.arange(0, output_subframes).unfold(
-        0, subframes_per_frame, subframe_step)
+    frame = torch.arange(0, output_subframes).unfold(0, subframes_per_frame, subframe_step)
     frame = signal.new_tensor(frame).long()  # signal may in GPU or CPU
     frame = frame.contiguous().view(-1)
 
     result = signal.new_zeros(*outer_dimensions, output_subframes, subframe_length)
-    result.index_add_(-2, frame, subframe_signal)
+    device_of_result = result.device
+    result.index_add_(-2, frame.to(device_of_result), subframe_signal)
     result = result.view(*outer_dimensions, -1)
     return result
 
