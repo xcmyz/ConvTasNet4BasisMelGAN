de7083942cc697b1184dcbf869c7d5af435b585f
xcmyz <xcmyz@outlook.com>
2021-06-09 23:01:34 +0800

    Update preprocessing

--- preprocess.py
+++ preprocess.py
@@ -12,36 +12,34 @@ from concurrent.futures import ProcessPoolExecutor
 
 
 def _process_utterance(in_path, out_path, index):
-    wav = torch.Tensor(audio.load_wav(in_path))
+    wav = torch.Tensor(audio.load_wav(in_path, hp.sample_rate, encode=False))
     noi = audio.add_noise(wav, quantization_channel=hp.quantization_channel)
     mix = wav.float() + noi
 
-    wav_name = 'wav-%05d.npy' % index
-    noi_name = 'noi-%05d.npy' % index
-    mix_name = "mix-%05d.npy" % index
+    wav_name = f"{index}.wav.npy"
+    noi_name = f"{index}.noi.npy"
+    mix_name = f"{index}.mix.npy"
 
     np.save(os.path.join(out_path, wav_name), wav.numpy(), allow_pickle=False)
     np.save(os.path.join(out_path, noi_name), noi.numpy(), allow_pickle=False)
     np.save(os.path.join(out_path, mix_name), mix.numpy(), allow_pickle=False)
 
 
+def get_pathfile():
+    with open("BZNSYP.txt", "w", encoding="utf-8") as f:
+        for filename in os.listdir(os.path.join("BZNSYP", "Wave")):
+            f.write(os.path.abspath(os.path.join("BZNSYP", "Wave", filename)) + "\n")
+
+
 if __name__ == "__main__":
-    futures = list()
+    # Get path in a file
+    get_pathfile()
     os.makedirs(hp.dataset_path, exist_ok=True)
-    executor = ProcessPoolExecutor(max_workers=cpu_count())
-
-    file_list = os.listdir(os.path.join("BZNSYP", "Wave"))
-    if hp.test_size != 0:
-        file_list = file_list[:hp.test_size]
-    # for index, filename in enumerate(file_list):
-    for index in tqdm(range(len(file_list))):
-        filename = file_list[index]
-        in_path = os.path.join("BZNSYP", "Wave", filename)
-        # futures.append(executor.submit(
-        #     partial(_process_utterance, in_path, hp.dataset_path, index+1)))
-        _process_utterance(in_path, hp.dataset_path, index + 1)
-
-        # if (index + 1) % 100 == 0:
-        #     print("Done", index + 1)
-
-    # [future.result() for future in tqdm(futures)]
+    with open("BZNSYP.txt", "r", encoding="utf-8") as f:
+        paths = f.readlines()
+        length = len(paths)
+        for i in tqdm(range(length)):
+            path = paths[i]
+            path = path[:-1]
+            index = path.split("/")[-1]
+            _process_utterance(os.path.join(path), hp.dataset_path, index)
