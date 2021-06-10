b2b8d14b8a9e299b969bdd5423991b184c671d62
xcmyz <xcmyz@outlook.com>
2021-06-10 12:05:49 +0800

    Update generator

--- generator.py
+++ generator.py
@@ -8,7 +8,7 @@ import numpy as np
 import hparams as hp
 
 from tqdm import tqdm
-from analysis import get_DNN
+from analysis import get_model
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -17,6 +17,9 @@ def autoencoder(model, wav):
     wav = torch.stack([wav]).to(device)
     with torch.no_grad():
         est_source, weight, weight_ = model.module.autoencode(wav)
+        if False:
+            test_wav = model.module.vocoder(weight_)
+            audio.save_wav(test_wav[0].numpy(), "test_.wav", hp.sample_rate)
 
     est_wav = est_source[0].cpu().numpy()
     weight = weight[0].cpu().numpy()
@@ -27,30 +30,24 @@ def autoencoder(model, wav):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--step', type=int, default=0)
-    parser.add_argument('--get_test', type=bool, default=False)
     args = parser.parse_args()
 
-    model = get_DNN(args.step)
+    model = get_model(args.step)
     weight = model.module.decoder.basis_signals.weight
     weight = weight.detach().numpy().astype(np.float32)
     np.save("basis_signal_weight.npy", weight, allow_pickle=False)
+    os.makedirs("generated", exist_ok=True)
     os.makedirs("weight", exist_ok=True)
     list_filename = list()
-    with open(os.path.join("LJSpeech-1.1", 'metadata.csv'), encoding='utf-8') as f:
-        for line in f.readlines():
-            parts = line.strip().split('|')
-            wav_path = os.path.join(
-                "LJSpeech-1.1", 'wavs', '%s.wav' % parts[0])
-            list_filename.append(wav_path)
-
-    if not args.get_test:
-        if hp.gen_size != 0:
-            list_filename = list_filename[:hp.gen_size]
-    else:
-        list_filename = [list_filename[0]] + list_filename[-6:]
-
-    for i in tqdm(range(len(list_filename))):
-        wav = audio.load_wav(list_filename[i])
-        wav = torch.Tensor(wav)
-        wav_, weight, weight_ = autoencoder(model, wav)
-        np.save(os.path.join("weight", str(i)+".npy"), weight_)
+    with open("BZNSYP.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        len_lines = len(lines)
+        for i in tqdm(range(len_lines)):
+            line = lines[i]
+            line = line[:-1]
+            wav = audio.load_wav(line, sample_rate=hp.sample_rate, encode=False)
+            wav = torch.Tensor(wav)
+            wav_, weight, weight_ = autoencoder(model, wav)
+            filename = line.split("/")[-1]
+            audio.save_wav(wav_, os.path.join("generated", filename), hp.sample_rate)
+            np.save(os.path.join("weight", f"{filename}.npy"), weight_)
